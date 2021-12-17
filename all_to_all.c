e8b064f1b3cb01e0e24e6129c89be60422797606
rrrongon <rubayetrongon@gmail.com>
2021-12-17 17:06:36 -0500

    - rank 0 collect & verbose

--- all_to_all.c
+++ all_to_all.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <stdbool.h>
 
 int main(int argc, char* argv[])
 
 {
+
+   int DEBUG_LOG =0; 
+   DEBUG_LOG = atoi(argv[1]);
+
     MPI_Init(&argc, &argv);
  
     int size;
@@ -26,11 +31,17 @@ int main(int argc, char* argv[])
     {
         my_values[i] = my_rank * size * 100 + i * 100;
     }
-    for (int i=0; i< size; i++)
-    printf("Process %d, my values = %d\n", my_rank, my_values[i]);
- 
+
+    if (DEBUG_LOG){
+        for (int i=0; i< size; i++){
+             printf("Process %d, my values = %d\n", my_rank, my_values[i]);
+        }
+    }
+
     int* buffer_recv = malloc(sizeof(int) * size);
     MPI_Alltoall(my_values, 1, MPI_INT, buffer_recv, 1, MPI_INT, MPI_COMM_WORLD);
+
+    bool passed = false;
 
     for (int i =0; i < size ; i++){
 	int my_buf_val = buffer_recv[i];
@@ -38,12 +49,46 @@ int main(int argc, char* argv[])
 	int global_val = all_values[global_val_pos];
 
 	if (my_buf_val != global_val){
-	    printf("Value MISMATCHED. Rank=%d, position=%d, received val=%d, actual val = %d\n", my_rank, i, my_buf_val, global_val);
+	    if(DEBUG_LOG)
+	        printf("Value MISMATCHED. Rank=%d, position=%d, received val=%d, actual val = %d\n", my_rank, i, my_buf_val, global_val);
+	    passed = false;
+	    break;
 	}else{
-	    printf("Value MATCHED. Rank=%d, position=%d, received val=%d, actual val = %d\n", my_rank, i, my_buf_val, global_val);
+	    if(DEBUG_LOG)
+	        printf("Value MATCHED. Rank=%d, position=%d, received val=%d, actual val = %d\n", my_rank, i, my_buf_val, global_val);
+	    passed = true;
 	}
 
 	//printf("Values collected on process %d: %d\n", my_rank, buffer_recv[i]);
+    }
+
+    if(my_rank==0){
+	MPI_Status status;
+	bool pass_val_recv = true;
+	bool global_decision = true;
+	if(passed){
+	    for(int rank=1; rank < size; rank++){
+	    	MPI_Recv(&pass_val_recv, 1, MPI_INT, rank, 0, MPI_COMM_WORLD, &status);
+		if(!pass_val_recv){
+		    global_decision = global_decision && pass_val_recv;
+		}
+	    }
+
+	    if(global_decision)
+		printf("TEST: PASS\n");
+	    else
+		printf("TEST: FAIL");
+	}else{
+	    printf("TEST: FAIL\n");
+	}
+    }else{
+	int passed_val = 0;
+	if(passed)
+	    passed_val = 1;
+	else
+	    passed_val = 0;
+
+	MPI_Send(&passed_val, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
  
     MPI_Finalize();
