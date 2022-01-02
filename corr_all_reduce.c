786877d29299d12642863e64547bff9b79fce0a9
rrrongon <rubayetrongon@gmail.com>
2022-01-01 20:49:29 -0500

    -LOG I/O

--- corr_all_reduce.c
+++ corr_all_reduce.c
@@ -1,9 +1,27 @@
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m" 
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
 #include <math.h>
 #include <stdbool.h>
 
+/*
+ *    Allreduce: Each process has subtask. They compute.
+ *    Each process calls allreduce routine mentioning an operation
+ *    Finally all process gets summed result
+ *
+ *    To check correctness: 
+ *    Each process generates data. Sums
+ *    Send generated data to root.
+ *    Root collect all input data and sum to get Expected Reference sum
+ *    Then processes also send what sum they received from Routine
+ *    Root checks if calculation is correctly done by routine and sent to processes
+ *      */
+
+/*Generating random numbers for input*/
 float *create_rand_nums(int num_elements) {
         float *rand_nums = (float *)malloc(sizeof(float) * num_elements);
         int i;
@@ -16,6 +34,12 @@ float *create_rand_nums(int num_elements) {
 
 int main(int argc, char** argv) {
 
+	int DEBUG_LOG =0; 
+	if(argv[1]==NULL)
+                DEBUG_LOG = 0;
+        else
+                DEBUG_LOG = atoi(argv[1]);
+
         int num_elements_per_proc = 1000; //atoi(argv[1]);
 
         MPI_Init(NULL, NULL);
@@ -25,42 +49,88 @@ int main(int argc, char** argv) {
         int world_size;
         MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-
+	/*GEnerate input for each process*/
         srand(time(NULL)*my_rank); // Seed the random number generator of processes uniquely
         float *rand_nums = NULL;
         rand_nums = create_rand_nums(num_elements_per_proc);
 
+	//completes subtask
         float local_sum = 0;
         int i;
         for (i = 0; i < num_elements_per_proc; i++) {
                 local_sum += rand_nums[i];
         }
 
+	//Call Routine and Exchange subtask to allreduce among all processes
         float global_sum;
         MPI_Allreduce(&local_sum, &global_sum, 1, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
 
         if (my_rank==0){
                 float global_sum_recv = 0;
                 MPI_Status status;
+		
+		/*Receives sub-task input data from all process to calculate Final sum for reference*/
+		int tag = 3;
+		float sum=0;
+		int cnt = num_elements_per_proc;
+		for(int rank=1; rank < world_size; rank++){
+			float *input_recv_buf = (float *)malloc(sizeof(float) * num_elements_per_proc);
+			MPI_Recv(input_recv_buf, cnt, MPI_FLOAT, rank, tag , MPI_COMM_WORLD, &status);
+			for (int j=0;j < cnt;j++)
+				sum += input_recv_buf[j];
 
-                bool passed = false;
+			if(DEBUG_LOG)
+				printf("Received input from RANK %d in Root\n", rank);
+		}
+		// add rank 0's own sum
+		sum += local_sum;
+		if(DEBUG_LOG)
+			printf("CALCULATED SUM:%f\n", sum);
 
+		/*
+ * 		Ask Process What they got from allreduce routine
+ *		Check routine sum with Expected sum with a 0.01 margin deviation
+ * 		*/
+		
+                bool passed = true;
+		tag = 1;
+		cnt = 1;
                 for (int rank=1; rank < world_size; rank++){
-                        MPI_Recv(&global_sum_recv, 1, MPI_FLOAT, rank, 1, MPI_COMM_WORLD, &status);
-                        if (global_sum_recv == global_sum){
-                                passed = true;
+                        MPI_Recv(&global_sum_recv, cnt, MPI_FLOAT, rank, tag, MPI_COMM_WORLD, &status);
+			if(DEBUG_LOG)
+				printf("RANK %d: ROUTINE SUM: %f, EXPECTED SUM %f\n ", rank, global_sum_recv, sum);
+
+			float diff = global_sum_recv - sum;
+			if(diff < 0)
+				diff = diff * (-1);
+			if(diff < 0.01){
+                                passed = passed && true;
                         }else{
-                                passed = false;
-                                break;
+                                passed = passed && false;
+                                if(DEBUG_LOG)
+					printf(RED"TEST: FAIL. RANK %d\n"RESET, rank);
                         }
                 }
+		/*Check for rank 0 too*/
+		float diff = global_sum_recv - sum;
+                if(diff < 0)
+                	diff = diff * (-1);
 
-                if(passed)
-                        printf("PASSED\n");
+                if(passed && (diff < 0.01))
+                        printf("TEST: PASS\n");
                 else
-                        printf("FAILED\n");
+                        printf(RED"TEST: FAIL\n"RESET);
         }else{
-                MPI_Send(&global_sum, 1, MPI_FLOAT, 0, 1, MPI_COMM_WORLD);
+		/*
+ *		Send input and Routine sum to rank 0 for checking
+ * 		*/
+		int dest_rank = 0;
+		int tag = 3;
+		MPI_Send(rand_nums, num_elements_per_proc, MPI_FLOAT, dest_rank, tag, MPI_COMM_WORLD);
+		
+		int cnt=1;
+		tag = 1;
+                MPI_Send(&global_sum,cnt , MPI_FLOAT, dest_rank, tag, MPI_COMM_WORLD);
         }
 
         MPI_Comm New_Comm;
@@ -110,12 +180,18 @@ int main(int argc, char** argv) {
                                 if(diff <0)
                                     diff = diff * (-1);
 
-                                if(diff>0.001)
+                                if(diff>0.001){
                                         subcom_global_pass = subcom_global_pass && 0;
-                                else
+					if(DEBUG_LOG)
+						printf(RED"SUBCOMM TEST: FAIL. NEWID:%d EXPECTED: %f, Routine: %f\n"RESET, rank, subcom_cal_global_sum, subcom_recv_num);
+                                }else{
                                         subcom_global_pass = subcom_global_pass && 1;
+				}
                         }
 
+			/*
+ *			Finally non-zero rank in COMMWORLD sends sub-decision to True root
+ * 			*/
                         if(my_rank ==0){
                                 int subcom_global_pass_recv;
                                 MPI_Recv (&subcom_global_pass_recv, 1, MPI_INT, 1, 3, MPI_COMM_WORLD, &subcom_status);
