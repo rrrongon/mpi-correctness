c9b29c7940eba4c12d64187ce2a37c5abc36d4c2
rrrongon <rubayetrongon@gmail.com>
2022-01-24 00:21:40 -0500

    -subcomm

--- corr_allgather.c
+++ corr_allgather.c
@@ -57,9 +57,9 @@ int main(int argc, char** argv) {
         MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
         /*
- *         Root generates input data to distribute task among all process
- *                 sub_rand_nums to accept sub-task input from root
- *                         */
+        Root generates input data to distribute task among all process
+ 	sub_rand_nums to accept sub-task input from root
+       */
         for (size_counter=0; size_counter<SIZES; size_counter++){
        
                 int num_elements_per_proc = sizes[size_counter] * 1024;
@@ -72,7 +72,7 @@ int main(int argc, char** argv) {
 
                 /*
  *                  Each process call scatter routine to accept sub-input from root
- *                                   */
+ *              */
 
                 MPI_Scatter(rand_nums, num_elements_per_proc, MPI_FLOAT, sub_rand_nums, num_elements_per_proc, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
@@ -85,10 +85,6 @@ int main(int argc, char** argv) {
 
                 if (my_rank == 0) {
                         
-                        /*
- *                         * Calculate all value average in root process to check final average received from sub-tasks
- *                                                 * */
-
                         MPI_Status status;
                         int global_pass, local_pass, rank, data_cnt,pos;
 			float v1,v2;
@@ -98,7 +94,8 @@ int main(int argc, char** argv) {
 			
                         for(rank=1; rank < world_size; rank++){
 				local_pass = 1;
-
+				/*Collect allgather data from all processes to check what they received.
+ * 				And check element by element*/
                                 MPI_Recv(ranks_recv_buf, data_cnt, MPI_FLOAT, rank, 0, MPI_COMM_WORLD, &status);
 
                                 for(pos=0; pos< data_cnt; pos++){
@@ -123,6 +120,30 @@ int main(int argc, char** argv) {
 
                         }
 
+			/*Check for rank 0 too*/
+			local_pass = 1;
+                        for(pos=0; pos< data_cnt; pos++){
+                                v1 = recv_buf[pos];
+                                v2 = rand_nums[pos];
+
+                                if(v1!=v2)
+                                        local_pass = local_pass && 0;
+                                else
+                                        local_pass = local_pass && 1;
+
+			}
+                        
+                        global_pass = global_pass && local_pass;
+				
+                        if(local_pass){
+                                if(DEBUG_LOG)
+                                        printf("RANK %d: PASS\n", my_rank);
+
+                        }else{
+                                        printf(RED"RANK: %d FAIL\n"RESET, my_rank);
+                        }
+
+
                         free(rand_nums);
                         free(ranks_recv_buf);
 
@@ -132,7 +153,7 @@ int main(int argc, char** argv) {
                                 printf(RED"TEST %d*1024 byte: FAIL\n"RESET, sizes[size_counter]);
 
                 }else{
-			                        /*Send calculated value received by routine to rank 0 to check*/
+			/*Send value received by routine to rank 0 to check*/
 			int data_cnt = num_elements_per_proc * world_size;
                         int tag = 0;
                         int dest_rank = 0;
@@ -178,7 +199,7 @@ int main(int argc, char** argv) {
                                 sub_global_pass=1;                        
                                 data_cnt = num_elements_per_proc * new_world_size;
 			
-                                for(rank=1; rank < sub_global_pass; rank++){
+                                for(rank=1; rank < new_world_size; rank++){
                                         sub_local_pass = 1;
 
                                         MPI_Recv(ranks_recv_buf, data_cnt, MPI_FLOAT, rank, 0, New_Comm, &status);
@@ -204,6 +225,28 @@ int main(int argc, char** argv) {
                                         }
 
                                 }
+				
+				sub_local_pass = 1;
+                                for(pos=0; pos< data_cnt; pos++){
+                                        v1 = recv_buf[pos];
+                                        v2 = rand_nums[pos];
+
+                                        if(v1!=v2)
+                                                sub_local_pass = sub_local_pass && 0;
+                                        else
+                                                sub_local_pass = sub_local_pass && 1;
+
+                                }
+                                
+                                sub_global_pass = sub_global_pass && sub_local_pass;
+                                        
+                                if(sub_local_pass){
+                                        if(DEBUG_LOG)
+                                                printf("NEW RANK %d: PASS\n", new_id);
+
+                                }else{
+                                                printf(RED"RANK: %d FAIL\n"RESET, new_id);
+                                }
 
                                 free(rand_nums);
                                 free(ranks_recv_buf);
@@ -213,6 +256,7 @@ int main(int argc, char** argv) {
                                 else
                                         printf(RED"SUBCOMM TEST %d*1024 byte: FAIL\n"RESET, sizes[size_counter]);
 
+				/*One of 2 new subcom roots, will send it work decision to global root to make final verdict*/	
                                 if(my_rank == 0){
                                         
                                         MPI_Status status;
@@ -233,6 +277,7 @@ int main(int argc, char** argv) {
                                 int dest_rank = 0;
                                 MPI_Send(recv_buf, data_cnt, MPI_FLOAT, dest_rank, tag, New_Comm);
                         }
+			MPI_Barrier(New_Comm);
                 }
         }
 
